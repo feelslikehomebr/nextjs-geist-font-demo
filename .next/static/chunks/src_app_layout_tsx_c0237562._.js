228dc674daebed64a83e44f539b2125affdca1a0
feelslikehomebr <71987338+feelslikehomebr@users.noreply.github.com>
2025-06-23 14:33:57 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1750700037429

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/[root-of-the-server]__4668e977._.js",
+  "static/chunks/[next]_internal_font_google_poppins_7e41551_module_3c590437.css"
+],
+    source: "dynamic"
+});
